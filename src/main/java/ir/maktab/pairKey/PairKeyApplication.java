63fc69956e066c6ba35608921a7adc08f8a699a9
n.akbari <n.akbari@mohaymen.ir>
2022-09-11 10:38:21 +0430

    Initial commit

---
+++ src/main/java/ir/maktab/pairKey/PairKeyApplication.java
@@ -0,0 +1,13 @@
+package ir.maktab.pairKey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PairKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/maktab/pairKey/PairKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktab.pairKey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
